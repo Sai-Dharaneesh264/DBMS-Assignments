1a0fa47c323ea3b3b76fcb6b4ae69d65c95367ae
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-03-24 18:14:59 +0530

    completed the sql query assignment 2

--- SQL Query Assignment-2/db.sql
+++ SQL Query Assignment-2/db.sql
@@ -42,3 +42,48 @@ INNER JOIN payment
 	ON rental.rental_id = payment.rental_id
 GROUP BY film.film_id
 ORDER BY COUNT(*);
+
+-- ---------------------------------------------------------- 
+SELECT SUM(payment.amount) AS TotalSalesByAnimationFilms
+FROM film
+INNER JOIN film_category
+ON film.film_id = film_category.film_id
+INNER JOIN category
+	ON film_category.category_id = category.category_id
+	AND category.name = 'animation'
+INNER JOIN inventory
+	ON film.film_id = inventory.film_id
+INNER JOIN rental
+	ON inventory.inventory_id = rental.inventory_id
+INNER JOIN payment
+	ON rental.rental_id = payment.rental_id;
+
+-- --------------- QUESTION 4 ------------------
+SELECT DISTINCT film.*, category.name AS film_category 
+FROM film
+INNER JOIN film_category
+	ON film.film_id = film_category.film_id
+INNER JOIN category
+	ON film_category.category_id = category.category_id
+INNER JOIN inventory
+	ON film.film_id = inventory.film_id
+INNER JOIN rental
+	ON inventory.inventory_id = rental.inventory_id
+INNER JOIN customer
+	ON rental.customer_id = customer.customer_id
+	AND customer.first_name = 'patricia'
+	AND customer.last_name ='johnson'
+ORDER BY (film.rental_rate * film.rental_duration) DESC LIMIT 3;
+
+-- --------------- QUESTION 5 ------------------
+SELECT film.* FROM film
+INNER JOIN inventory
+	ON film.film_id = inventory.film_id
+INNER JOIN rental
+	ON inventory.inventory_id = rental.inventory_id
+INNER JOIN customer
+	ON rental.customer_id = customer.customer_id
+	AND customer.first_name = 'susan'
+	AND customer.last_name ='wilson'
+AND rating = 'r'
+ORDER BY film.film_id;
